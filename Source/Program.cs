83095f0766d5c9e0e8e868a4c67a1ccb0b674926
Kirill <kglad67@gmail.com>
2021-07-04 00:50:58 +0300

    freeway removing

--- Source/Program.cs
+++ Source/Program.cs
@@ -2,11 +2,5 @@
 
 namespace Source
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    
 }
--- Source/Task2.cs
+++ Source/Task2.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Tasks
 {
   public class Task2
   {
 
-    public static void Maint(string[] args)
+    public static void Main(string[] args)
     {
       Good iPhone12 = new Good("IPhone 12");
       Good iPhone11 = new Good("IPhone 11");
@@ -28,6 +29,7 @@ namespace Tasks
 //Вывод всех товаров в корзине
 
       Console.WriteLine(cart.Order().Paylink);
+      Console.ReadKey();
     }
 
   }
@@ -42,16 +44,25 @@ namespace Tasks
     }
   }
 
-  public class Warehouse
+  public interface ICellStorage
+  {
+    IReadOnlyList<Cell> Cells { get; }
+
+    void Delive(Good good, int count);
+
+  }
+
+  public class Warehouse : ICellStorage
   {
     List<Cell> _cells = new List<Cell>();
+    List<Cell> _reserved = new List<Cell>();
     
     
     public IReadOnlyList<Cell> Cells => _cells;
 
     public void Delive(Good good, int count)
     {
-      int index = _cells.FindIndex(x => x.Good.Name == good.Name);
+      int index = GetIndex(good);
 
       if (index != -1)
         _cells[index] = _cells[index].AddItem(count);
@@ -59,15 +70,44 @@ namespace Tasks
        _cells.Add(new Cell(good, count));
     }
 
-    public bool CanExtract(Good good, int count)
+    public bool CanReserve(Good good, int count)
     {
-      throw new NotImplementedException();
+      int index = GetIndex(good);
+
+      return index != -1 && count <= _cells[index].Count;
     }
 
-    public void Extract(Good good, int count)
+    public void Reserve(Good good, int count)
     {
-      throw new NotImplementedException();
+      int index = GetIndex(good);
+      _cells[index] = _cells[index].RemoveItem(count);
+      
+      int indexInReserved = GetIndexInReserved(good);
+
+      if (indexInReserved != -1)
+        _reserved[indexInReserved] = _cells[indexInReserved].AddItem(count);
+      else
+        _reserved.Add(new Cell(good, count));
     }
+
+    public void Remove(Good good, int count)
+    {
+      int indexInReserved = GetIndexInReserved(good);
+
+      if (indexInReserved == -1)
+        throw new InvalidOperationException("Can't remove not reserved item");
+      
+      if (_reserved[indexInReserved].Count < count)
+        throw new InvalidOperationException("Can't remove more when reserved");
+      
+      
+    }
+
+    private int GetIndex(Good good) => 
+      _cells.FindIndex(x => x.Good.Name == good.Name);
+
+    private int GetIndexInReserved(Good good) => 
+      _reserved.FindIndex(x => x.Good.Name == good.Name);
 
   }
   public struct Cell
@@ -91,6 +131,17 @@ namespace Tasks
 
       return new Cell(Good, Count + count);
     }
+    
+    public Cell RemoveItem(int count)
+    {
+      if (count <= 0)
+        throw new ArgumentOutOfRangeException(nameof(count));
+      
+      if (count > Count)
+        throw new InvalidOperationException("Can't remove more than contain cell");
+
+      return new Cell(Good, Count - count);
+    }
   }
 
   internal class Shop
@@ -100,7 +151,6 @@ namespace Tasks
     public Shop(Warehouse warehouse)
     {
       _warehouse = warehouse;
-      throw new System.NotImplementedException();
     }
 
     public Cart Cart() => 
@@ -111,42 +161,77 @@ namespace Tasks
   public class Cart
   {
     private readonly Warehouse _warehouse;
-    private readonly Warehouse _cartWarehouse;
+    private readonly Order _order = new Order();
 
     public Cart(Warehouse warehouse)
     {
       _warehouse = warehouse;
-      _cartWarehouse = new Warehouse();
     }
 
     public void Add(Good good, int count)
     {
-      if (!_warehouse.CanExtract(good, count))
-        throw new InvalidOperationException();
+      if (!_warehouse.CanReserve(good, count))
+        throw new InvalidOperationException("Good is not contains in warehouse or not enough amount");
       
-      _cartWarehouse.Delive(good, count);
+      _warehouse.Reserve(good, count);
+      _order.CellStorage.Delive(good, count);
     }
 
     public Order Order()
     {
+      foreach (Cell cell in _order.CellStorage.Cells)
+      {
+        _warehouse.Remove(cell.Good, cell.Count);
+      }
+      return _order;
       foreach (Cell cell in _cartWarehouse.Cells) 
-        _warehouse.Extract(cell.Good, cell.Count);
+        _warehouse.Reserve(cell.Good, cell.Count);
 
       return new Order(_cartWarehouse.Cells);
     }
-
   }
 
   public class Order
   {
+    public ICellStorage CellStorage { get; } = new Warehouse();
     public IReadOnlyList<Cell> CartWarehouseCells { get; }
 
-    public Order(IReadOnlyList<Cell> cartWarehouseCells)
+    private IPaylinkProvider _paylinkProvider = new RandomPaylinkProvider(10);
+
+    public Order()
     {
-      CartWarehouseCells = cartWarehouseCells;
+      Paylink = _paylinkProvider.GetLink();
     }
 
     public object Paylink { get; set; }
+  }
 
+  public class RandomPaylinkProvider : IPaylinkProvider
+  {
+    private readonly int _linkLenght;
+    
+    private Random _random = new Random();
+    
+    
+    public RandomPaylinkProvider(int linkLenght)
+    {
+      _linkLenght = linkLenght;
+    }
+    
+    public string GetLink() => 
+      GetRandomString();
+
+    private string GetRandomString()
+    {
+      string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      
+      return new string(Enumerable.Repeat(chars, _linkLenght)
+        .Select(s => s[_random.Next(s.Length)]).ToArray());
+    }
+  }
+
+  public interface IPaylinkProvider
+  {
+    string GetLink();
   }
 }
