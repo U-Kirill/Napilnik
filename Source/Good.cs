d67bd1de296ad31e951f7acc0af9c5ade5278a8e
Kirill <kglad67@gmail.com>
2021-07-06 04:38:46 +0300

    Add check for valid name

--- Source/Good.cs
+++ Source/Good.cs
@@ -10,8 +10,15 @@ namespace Napilnik.Encapsulation
     {
       if (name == null)
         throw new NullReferenceException(nameof(name));
-      
+
+      if (!IsValidName(name))
+        throw new ArgumentOutOfRangeException($"{nameof(name)} is null or emply. Value: {name}");
+
       Name = name;
     }
+
+    private bool IsValidName(string name) =>
+      !string.IsNullOrEmpty(name?.Replace(" ", string.Empty));
+
   }
 }
