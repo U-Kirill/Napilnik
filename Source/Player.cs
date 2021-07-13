4de54b4914f9c992bf0e2107c3a04e1b4d5739c7
Kirill <kglad67@gmail.com>
2021-07-13 15:48:13 +0300

    task complete

---
+++ Source/Player.cs
@@ -0,0 +1,18 @@
+﻿namespace Source
+{
+  public class Player
+  {
+    public Player(string name, int coins, int army)
+    {
+      Name = name;
+      Coins = coins;
+      Army = army;
+    }
+
+    public string Name { get; }
+
+    public int Coins { get; }
+
+    public int Army { get; }
+  }
+}
